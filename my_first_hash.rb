5735b886cc6164bbd57d5f636669a8c5b206de14
Emily Asch <e.asch@me.com>
2018-10-15 19:23:30 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -34,11 +34,7 @@ def adding
                 }
   # add 2 muskets to the shipping_manifest hash below
 
-
-
   # add 4 gun powder to the shipping_manifest hash below
-
-
 
   # return the shipping_manifest hash below
 shipping_manifest= ["muskets"] = 2
