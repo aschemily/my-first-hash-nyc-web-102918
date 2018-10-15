498fc847f1cf1588453c75facec7d86921b9bbac
Emily Asch <e.asch@me.com>
2018-10-15 19:22:51 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -14,17 +14,17 @@ the_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil painting
 return the_manifest
 end
 
-# def retrieval
-#   shipping_manifest = {
-#                   "whale bone corsets" => 5, 
-#                   "porcelain vases" => 2, 
-#                   "oil paintings" => 3
-#                 }
+def retrieval
+  shipping_manifest = {
+                  "whale bone corsets" => 5, 
+                  "porcelain vases" => 2, 
+                  "oil paintings" => 3
+                }
 
-#   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-# return shipping_manifest["oil paintings"]
+  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
+return shipping_manifest["oil paintings"]
 
-# end
+end
 
 # def adding
 #   shipping_manifest = {
